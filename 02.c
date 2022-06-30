ece4606b1526d93d76fdf721e96072eb2faf8903
Arthur de Camargo Alves <arthur_rokero@hotmail.com>
2022-06-30 16:57:17 -0300

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+    float vet[6] = {8, 9, 7, 4, 5, 6}, media1, media2;
+
+    media1 = vet[0] + vet[1] + vet[2];
+    media2 = vet[3] + vet[4] + vet[5];
+
+    printf("Média de 8, 9 e 7: %.2f\n", media1);
+    printf("Média de 4, 5 e 6: %.2f\n", media2);
+    printf("Soma das médias: %.2f\n", (media + media));
+    printf("Média das médias: %.2f\n", ((media + media) / 2));
+}
